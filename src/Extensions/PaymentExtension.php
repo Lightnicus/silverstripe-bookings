113fa1480e2dd0de0e432226688d50e05ceda421
Ed <ed@hothouse.co.nz>
2025-08-17 10:07:32 +0200

    tweaks

--- src/Extensions/PaymentExtension.php
+++ src/Extensions/PaymentExtension.php
@@ -7,6 +7,7 @@ use Sunnysideup\Bookings\Model\Booking;
 
 /**
  * Extension for SilverStripe\Omnipay\Model\Payment to add relationship back to Booking
+ * and enhance the admin display with transaction reference
  *
  * @property Payment|PaymentExtension $owner
  * @property int $BookingID
@@ -17,4 +18,35 @@ class PaymentExtension extends DataExtension
     private static $has_one = [
         'Booking' => Booking::class,
     ];
+    
+    /**
+     * Update the summary fields to include the transaction reference (charge ID)
+     */
+    public function updateSummaryFields(&$fields)
+    {
+        // Add TransactionReference after Gateway
+        $newFields = [];
+        foreach ($fields as $key => $label) {
+            $newFields[$key] = $label;
+            if ($key === 'GatewayTitle') {
+                $newFields['getTransactionReferenceFormatted'] = 'Charge ID';
+            }
+        }
+        $fields = $newFields;
+    }
+    
+    /**
+     * Get a formatted transaction reference for display
+     * 
+     * @return string
+     */
+    public function getTransactionReferenceFormatted()
+    {
+        $reference = $this->owner->TransactionReference;
+        if (empty($reference)) {
+            return 'No charge ID';
+        }
+        
+        return $reference;
+    }
 }
